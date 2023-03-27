1181b5945462fc7feeba5b46ccf6cf80a3d09731
Konstantin <k.elfimow@gmail.com>
2023-03-27 14:02:36 +0300

    Initial commit!

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 from PIL import Image
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
 import warnings
 warnings.filterwarnings(action='once')
-
-large = 22
-med = 16
-small = 12
-params = {'axes.titlesize': large,
-          'legend.fontsize': med,
-          'figure.figsize': (16, 10),
-          'axes.labelsize': med,
-          'axes.titlesize': med,
-          'xtick.labelsize': med,
-          'ytick.labelsize': med,
-          'figure.titlesize': large}
-plt.rcParams.update(params)
-plt.style.use('seaborn-whitegrid')
 
 encoding: str = 'utf-8'
 
